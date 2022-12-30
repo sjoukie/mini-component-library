d0b020af94ba9f4a0471db257e02cbf3f415b8af
sjoukie <sjoukje.ijlstra@reaktor.com>
2022-12-30 14:29:00 +0100

    start of icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,45 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16, 
+    borderThickness: 1,
+    height: 24
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36
+  }
+};
+
 const IconInput = ({
   label,
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+  const styles = STYLES[size];
+
+  return (
+    <form>
+      <Icon id={'search'} strokeWidth={1} size={16} />
+      <input type="search" placeholder='Search...'></input>
+    </form>
+  );
 };
+
+const Form = styled.form`
+  border-color: transparent;
+`;
+
+const InputBase = styled.input`
+  font-size: 18px;
+  font: Roboto;
+`;
 
 export default IconInput;
